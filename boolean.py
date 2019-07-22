3b962991d733c2a7f528f95d7d99dff8546620d9
elainechan <elaine.chan@outlook.com>
2019-07-22 00:00:32 -0400

    __next__

--- boolean.py
+++ boolean.py
@@ -5,23 +5,27 @@ class BooleanSearch:
         self.not_matches = self.not_search(term1_postings, term2_postings)
 
     def and_search(self, term1_postings, term2_postings):
-        term1_doc = term1_postings.next()
-        term2_doc = term2_postings.next()
+        term1_iter = iter(term1_postings)
+        term2_iter = iter(term2_postings)
+        term1_doc = term1_postings.__next__()
+        term2_doc = term2_postings.__next__()
 
         matches = []
         while term1_doc != None and term2_doc != None:
             if term1_doc == term2_doc:
                 matches.append(term1_doc)
             elif term1_doc < term2_doc:
-                term1_doc = term1_postings.next()
+                term1_doc = term1_iter.__next__()
             else:
-                term2_doc = term2_postings.next()
+                term2_doc = term2_iter.__next__()
         return matches
 
     def or_search(self, term1_postings, term2_postings):
         """Get union of all documents in both lists"""
-        term1_doc = term1_postings.next()
-        term2_doc = term2_postings.next()
+        term1_iter = iter(term1_postings)
+        term2_iter = iter(term2_postings)
+        term1_doc = term1_iter.__next__()
+        term2_doc = term2_iter.__next__()
 
         # Get matches in term1_doc
         matches = []
@@ -29,14 +33,13 @@ class BooleanSearch:
             if term1_doc == term2_doc:
                 matches.append(term1_doc)
             elif term1_doc < term2_doc:
-                term1_doc = term1_postings.next()
+                term1_doc = term1_postings.__next__()
             else:
-                term2_doc = term2_postings.next()
+                term2_doc = term2_postings.__next__()
         return matches
 
-    def not_search(self):
+    def not_search(self, term1_postings, term2_postings):
         """Get document IDs in between and_search matches.
         """
         matches = []
         return matches
-
