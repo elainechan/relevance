a5f54815904774e77c8cf2285e9b7def89db4dd2
elainechan <elaine.chan@outlook.com>
2019-07-22 22:16:03 -0400

    adds packaging.

---
+++ main.py
@@ -0,0 +1,7 @@
+from relevance.boolean import BooleanSearch
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='relevance',
+      version='0.1',
+      description='Search',
+      url='http://github.com/elainechan/relevance',
+      author='Elaine Chan',
+      license='MIT',
+      packages=find_packages()
+)
